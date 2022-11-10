f2dcb6ca8d2e3ade8089b6249301f2bbaafdc601
Sandaru <180321j@uom.lk>
2022-11-10 07:35:33 +0530

    tikinter started

--- main.py
+++ main.py
@@ -4,21 +4,20 @@ from coincurve import PrivateKey
 from bip44 import Wallet
 from bip44.utils import get_eth_addr
 from bip32 import BIP32, HARDENED_INDEX
-bip32 = BIP32.from_seed(bytes.fromhex("01"))
 
 
 #generate the random value 
-rand = random.getrandbits(128)
-print("Random number ",rand)
+randomNumber = random.getrandbits(128)
+print("Random number ",randomNumber)
 
 #convert the random value into byte array
 def bitstring_to_bytes(s):
     v = int(s, 2)
-    b = bytearray()
+    bytes_arr = bytearray()
     while v:
-        b.append(v & 0xff)
+        bytes_arr.append(v & 0xff)
         v >>= 8
-    return bytes(b[::-1])
+    return bytes(bytes_arr[::-1])
 
 
 def access_bit(data, num):
@@ -31,12 +30,14 @@ def byte_to_bit(seed):
     for i in range(len(seed)*8):
         bitSeed=bitSeed+str(access_bit(seed,i))
     return bitSeed
-print(hex(int(byte_to_bit(bip32.privkey), 2)))
 
-entropy= bitstring_to_bytes(bin(rand))
+
+entropy= bitstring_to_bytes(bin(randomNumber))
+
 print("entropy in byte array:::",entropy)
 #generate mnemonic words 
 mnemonic =bip39.encode_bytes(entropy)
+print(type(mnemonic))
 print("mnemonic-words:::",mnemonic)
 
 #generate 512 bit seed
@@ -45,22 +46,52 @@ seed= bip39.phrase_to_seed(mnemonic)
 print("512 bit seed::",byte_to_bit(seed))
 
 #generate seed using the mnemonic 
-d= bip39.decode_phrase(mnemonic)
-print("regenerated entropy using the mnemonic",d)
+decoded_phrase= bip39.decode_phrase(mnemonic)
+print("regenerated entropy using the mnemonic",decoded_phrase)
 
 # #generate entropy using the mnemonic
 # entropy= bip39.get_entropy_bits(12)
 # print(entropy)
 
+bip32 = BIP32.from_seed(seed)
+print(hex(int(byte_to_bit(bip32.privkey))))
 
-passphrase="12345"
-w=Wallet(mnemonic,"english",passphrase)
-sk, pk = w.derive_account("eth", account=0)
-# sk = PrivateKey(sk)
-privateKey=hex(int(byte_to_bit(sk), 2))
-publicKey=hex(int(byte_to_bit(pk), 2))
-print("private key of the wallet ",privateKey)
-print("public key of the wallet",publicKey)
 
-address= get_eth_addr(pk)
-print(address)
+extendedPrivateKey=bip32.get_xpriv_from_path("m/44'/0'/0'")
+print("Extended private key: ",extendedPrivateKey)
+
+privateKey= bip32.get_privkey_from_path("m/44'/0'/0'")
+print("Private key of the given path: ",hex(int(byte_to_bit(privateKey),2)))
+
+publicKey= bip32.get_pubkey_from_path("m/44'/0'/0'")
+print("public key of the given path: ",hex(int(byte_to_bit(publicKey),2)))
+
+from tkinter import *
+from tkinter import ttk
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Generate Entropy", command=root.destroy).grid(column=1, row=1)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.mainloop()
+
+
+
+
+# passphrase=""
+# w=Wallet(mnemonic,"english",passphrase)
+# sk, pk = w.derive_account("eth", account=0)
+# # sk = PrivateKey(sk) 
+# privateKey=hex(int(byte_to_bit(sk), 2))
+# publicKey=hex(int(byte_to_bit(pk), 2))
+# print("private key of the wallet ",privateKey)
+# print("public key of the wallet",publicKey)
+
+# address= get_eth_addr(pk)
+# print(address)
+
+
+
+
+
