af6c91f2522648ace0a9d65c6af1d2f6d32e3c22
Sandaru <180321j@uom.lk>
2022-11-14 18:53:49 +0530

    Initial UI created

--- interface.py
+++ interface.py
@@ -1,79 +1,52 @@
-import bip39 
-import random
-from bip32 import BIP32, HARDENED_INDEX
-from tkinter import *
-from tkinter import ttk
 
-#convert the random value into byte array
-def bitstring_to_bytes(s):
-    v = int(s, 2)
-    bytes_arr = bytearray()
-    while v:
-        bytes_arr.append(v & 0xff)
-        v >>= 8
-    return bytes(bytes_arr[::-1])
+# from tkinter import *
+# from tkinter import ttk
+# name=""
+# def changeName():
+#     name= "changed"
+#     return
 
 
-def access_bit(data, num):
-    base = int(num // 8)
-    shift = int(num % 8)
-    return (data[base] >> shift) & 0x1
 
-def byte_to_bit(seed):
-    bitSeed=""
-    for i in range(len(seed)*8):
-        bitSeed=bitSeed+str(access_bit(seed,i))
-    return bitSeed
 
-#generate the Entropy
-def generateEntropy():
-    randomNumber = random.getrandbits(128)
-    print("Random number ",randomNumber)
-    return randomNumber
-#Entropy as bytes
-def entropyAsBytes(randomNumber):
-    entropy= bitstring_to_bytes(bin(randomNumber))
-    return entropy
 
-##generate mnemonic words 
-def generateMnemonic(entropy):
-    mnemonic =bip39.encode_bytes(entropy)
-    return mnemonic
+# root = Tk()
+# frm = ttk.Frame(root, padding=20)
+# frm.grid()
+# ttk.Label(frm, text="Experiment").grid( row=0)
+# ttk.Button(frm, text="Generate Entropy", command=changeName).grid(column=0, row=1)
+# ttk.Label(frm, text=name).grid(column=0, row=2)
+# ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=2)
+# root.mainloop()
 
-#generate 512 bit seed
-def generateBitSeed(mnemonicPhrase):
-    seed= bip39.phrase_to_seed(mnemonicPhrase)
-    return seed
+import tkinter as tk
 
-#generate seed using the mnemonic 
-def regenerateEntropy(mnemonic):
-    decoded_phrase= bip39.decode_phrase(mnemonic)
-    return decoded_phrase
+root= tk.Tk()
 
-def createBIP32(seed):
-    bip32 = BIP32.from_seed(seed)
-    return bip32
+canvas1 = tk.Canvas(root, width=400, height=300, relief='raised')
+canvas1.pack()
 
-def getExtendedPrivateKEyFromPath(bip32,path="m/44'/0'/0'"):
-    extendedPrivateKey=bip32.get_xpriv_from_path(path)
-    return extendedPrivateKey
+label1 = tk.Label(root, text='Calculate the Square Root')
+label1.config(font=('helvetica', 14))
+canvas1.create_window(200, 25, window=label1)
 
-def getExtendedPublicKeyFromPath(bip32,path="m/44'/0'/0'"):
-    publicKey= bip32.get_pubkey_from_path(path)
-    return publicKey 
- 
+label2 = tk.Label(root, text='Type your Number:')
+label2.config(font=('helvetica', 10))
+canvas1.create_window(200, 100, window=label2)
 
-root = Tk()
-frm = ttk.Frame(root, padding=20)
-frm.grid()
-ttk.Label(frm, text="Experiment").grid( row=0)
-ttk.Button(frm, text="Generate Entropy", command=generateEntropy).grid(column=0, row=1)
-ttk.Button(frm, text="Generate Mnemonic", command=generateEntropy).grid(column=0, row=2)
-ttk.Button(frm, text="Generate seed", command=generateEntropy).grid(column=0, row=3)
-ttk.Button(frm, text="Generate Extended Privatekey", command=generateEntropy).grid(column=0, row=4)
-ttk.Button(frm, text="Generate Extended Public Key", command=generateEntropy).grid(column=0, row=5)
-ttk.Button(frm, text="Generate Private Key", command=generateEntropy).grid(column=0, row=6)
-ttk.Button(frm, text="Generate Entropy From The Mnemonic", command=generateEntropy).grid(column=0, row=7)
-ttk.Button(frm, text="Generate Public Key", command=generateEntropy).grid(column=0, row=8)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=2)
+entry1 = tk.Entry(root) 
+canvas1.create_window(200, 140, window=entry1)
+
+def get_square_root():
+    x1 = entry1.get()
+    
+    label3 = tk.Label(root, text='The Square Root of ' + x1 + ' is:', font=('helvetica', 10))
+    canvas1.create_window(200, 210, window=label3)
+    
+    label4 = tk.Label(root, text=float(x1)**0.5, font=('helvetica', 10, 'bold'))
+    canvas1.create_window(200, 230, window=label4)
+    
+button1 = tk.Button(text='Get the Square Root', command=get_square_root, bg='brown', fg='white', font=('helvetica', 9, 'bold'))
+canvas1.create_window(200, 180, window=button1)
+
 root.mainloop()
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from bip44.utils import get_eth_addr
 from bip32 import BIP32, HARDENED_INDEX
 from tkinter import *
 from tkinter import ttk
+import tkinter as tk
 
 #generate the random value 
 
@@ -18,8 +19,9 @@ def generateEntropy():
 
 
 
-#convert the random value into byte array
+#convert functions (bit to byteArray and byteArray to bit) 
 def bitstring_to_bytes(s):
+    print(s)
     v = int(s, 2)
     bytes_arr = bytearray()
     while v:
@@ -44,14 +46,16 @@ def byte_to_bit(seed):
 #generate mnemonic words 
 def generateMnemonic(entropy):
     mnemonic =bip39.encode_bytes(entropy)
+    print(mnemonic)
     return mnemonic
 
 
 
 
 #generate 512 bit seed
-def generateBitSeed(mnemonicPhrase):
-    seed= bip39.phrase_to_seed(mnemonicPhrase)
+def generateBitSeed(mnemonicPhrase,password):
+    seed= bip39.phrase_to_seed(mnemonicPhrase,password)
+    print("512 bit seed as a byte array: ",seed)
     print("512 bit seed::",byte_to_bit(seed))
     return seed
 # seed= bip39.phrase_to_seed(mnemonic)
@@ -65,68 +69,225 @@ def regenerateEntropy(mnemonic):
     return decoded_phrase
 
 
-# #generate entropy using the mnemonic
-# entropy= bip39.get_entropy_bits(12)
-# print(entropy)
 
+
+#create BIP32 object
 def createBIP32(seed):
     bip32 = BIP32.from_seed(seed)
     return bip32
 
 
-# print(hex(int(byte_to_bit(bip32.privkey))))
-
-
-# extendedPrivateKey=bip32.get_xpriv_from_path("m/44'/0'/0'")
-# print("Extended private key: ",extendedPrivateKey)
-
-# privateKey= bip32.get_privkey_from_path("m/44'/0'/0'")
-# print("Private key of the given path: ",hex(int(byte_to_bit(privateKey),2)))
-
-# publicKey= bip32.get_pubkey_from_path("m/44'/0'/0'")
-# print("public key of the given path: ",hex(int(byte_to_bit(publicKey),2)))
+#create extended private keys and public keys for the default path
 def getExtendedPrivateKEyFromPath(bip32,path="m/44'/0'/0'"):
     extendedPrivateKey=bip32.get_xpriv_from_path(path)
     print("Extended Private Key",extendedPrivateKey)
     return extendedPrivateKey
 
+def getExtendedPublicKeyFromPath(bip32,path="m/44'/0'/0'"):
+    extendedPublicKey= bip32.get_xpub_from_path(path)
+    print("Extended Private Key",extendedPublicKey)
+    return extendedPublicKey 
+ 
+#create public keys and the private keys for a given path 
 def getPublicKeyFromPath(bip32,path="m/44'/0'/0'"):
     publicKey= bip32.get_pubkey_from_path(path)
     print("public key of the given path: ",path,hex(int(byte_to_bit(publicKey),2)))
-    return publicKey 
- 
+    publicKeyHex=hex(int(byte_to_bit(publicKey),2))
+    return publicKeyHex
 
-def main():
+def getPrivateKEyFromPath(bip32,path="m/44'/0'/0'"):
+    privateKey=bip32.get_privkey_from_path(path)
+    print("Private key of the given path: ",path,hex(int(byte_to_bit(privateKey),2)))
+    privateKeyHex=hex(int(byte_to_bit(privateKey),2))
+    return privateKeyHex
+
+
+
+
+def main(password=""):
     entropy=generateEntropy()
     mnemonic=generateMnemonic(entropy)
-    seed=generateBitSeed(mnemonic)    
-    regeneratedEntropy=regenerateEntropy(mnemonic)
+    seed=generateBitSeed(mnemonic,password)    
+    regeneratedEntropy=regenerateEntropy(mnemonic) #not used for now just for testing
     bip32 =createBIP32(seed)
     xprv=getExtendedPrivateKEyFromPath(bip32,"m/44'/0'/0'")
+    xpub=getExtendedPublicKeyFromPath(bip32,"m/44'/0'/0'")
     
-    pub=getPublicKeyFromPath(bip32,"m/44'/0'/0'")
-    
+    prv1=getPrivateKEyFromPath(bip32,"m/44'/0'/0'/0")
+    pub1=getPublicKeyFromPath(bip32,"m/44'/0'/0'/0")
+
+    prv2=getPrivateKEyFromPath(bip32,"m/44'/0'/0'/1")
+    pub2=getPublicKeyFromPath(bip32,"m/44'/0'/0'/1")
+
+    prv3=getPrivateKEyFromPath(bip32,"m/44'/0'/0'/2")
+    pub3=getPublicKeyFromPath(bip32,"m/44'/0'/0'/2")
+
+    return entropy,mnemonic,seed,bip32,xprv,xpub,prv1,pub1,prv2,pub2,prv3,pub3
 
 
+
+
+root= tk.Tk()
+
+canvas1 = tk.Canvas(root, width=1500, height=700, relief='raised')
+canvas1.pack()
+
+label1 = tk.Label(root, text='Generating Private keys')
+label1.config(font=('helvetica', 14))
+canvas1.create_window(200, 25, window=label1)
+
+label2 = tk.Label(root, text='Type your Password :')
+label2.config(font=('helvetica', 10))
+canvas1.create_window(50, 100,anchor=W,window=label2)
+
+entry1 = tk.Entry(root) 
+canvas1.create_window(250, 100, window=entry1)
+
+#result viewer
+def viewer(bitEntropy,mnemonic,bitSeed,bip32,xprv,xpub,prv1,pub1,prv2,pub2,prv3,pub3):
+    label3 = tk.Label(root, text='The entropy ' + ' is:', font=('helvetica', 10))
+    canvas1.create_window(50, 250,anchor=W, window=label3)
+   
+    label4 = tk.Label(root, text=bitEntropy, font=('helvetica', 10, 'bold'))
+    canvas1.create_window(100, 250,width=1450,anchor=W, window=label4)
+
+    label5 = tk.Label(root, text='Mnemonic Phrase: ', font=('helvetica', 10))
+    canvas1.create_window(50, 300,anchor=W, window=label5)
+
+    label6 = tk.Label(root, text=mnemonic, font=('helvetica', 10, 'bold'))
+    canvas1.create_window(200, 300,width=550,anchor=W, window=label6)
+
+    label7 = tk.Label(root, text='----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------', font=('helvetica', 10))
+    canvas1.create_window(50, 350,anchor=W, window=label7)
+
+    # label7 = tk.Label(root, text='Seed: ', font=('helvetica', 10))
+    # canvas1.create_window(50, 350,anchor=W, window=label7)
+    
+    # label8 = tk.Label(root, text=bitSeed, font=('helvetica', 10, 'bold'))
+    # canvas1.create_window(150, 350,width=1450,anchor=W, window=label8)
+
+    label9 = tk.Label(root, text='Extended Private key: ', font=('helvetica', 10))
+    canvas1.create_window(50, 400,anchor=W, window=label9)
+    
+    label10 = tk.Label(root, text=xprv, font=('helvetica', 10, 'bold'))
+    canvas1.create_window(100, 400,width=1400,anchor=W, window=label10)
+
+    label11 = tk.Label(root, text='Extended Public key: ', font=('helvetica', 10))
+    canvas1.create_window(50, 450,anchor=W, window=label11)
+    
+    label12 = tk.Label(root, text=xpub, font=('helvetica', 10, 'bold'))
+    canvas1.create_window(100, 450,width=1400,anchor=W, window=label12)
+
+    # private key for the m/44/0/0/0 path 
+
+    label13 = tk.Label(root, text="Private key for m/44'/0'/0'/0: ", font=('helvetica', 10))
+    canvas1.create_window(50, 500,anchor=W, window=label13)
+    
+    label14 = tk.Label(root, text=prv1, font=('helvetica', 10, 'bold'))
+    canvas1.create_window(250, 500,width=800,anchor=W, window=label14)
+    #public key for the m/44/0/0/0 path 
+    label15 = tk.Label(root, text="Public key for m/44'/0'/0'/0: ", font=('helvetica', 10))
+    canvas1.create_window(50, 550,anchor=W, window=label15)
+    
+    label16 = tk.Label(root, text=pub1, font=('helvetica', 10, 'bold'))
+    canvas1.create_window(250, 550,width=800,anchor=W, window=label16)
+
+    #private key for the m/44/0/0/1 path 
+
+    label17 = tk.Label(root, text="Private key for m/44'/0'/0'/1: ", font=('helvetica', 10))
+    canvas1.create_window(50, 600,anchor=W, window=label17)
+    
+    label18 = tk.Label(root, text=prv2, font=('helvetica', 10, 'bold'))
+    canvas1.create_window(250, 600,width=800,anchor=W, window=label18)
+    #public key for the m/44/0/0/1 path 
+    label18 = tk.Label(root, text="Public key for m/44'/0'/0'/1: ", font=('helvetica', 10))
+    canvas1.create_window(50, 650,anchor=W, window=label18)
+    
+    label20 = tk.Label(root, text=pub2, font=('helvetica', 10, 'bold'))
+    canvas1.create_window(250, 650,width=800,anchor=W, window=label20)
+
+    #private key for the m/44/0/0/2 path 
+
+    label21 = tk.Label(root, text="Private key for m/44'/0'/0'/2: ", font=('helvetica', 10))
+    canvas1.create_window(50, 700,anchor=W, window=label21)
+    
+    label22 = tk.Label(root, text=prv3, font=('helvetica', 10, 'bold'))
+    canvas1.create_window(250, 700,width=800,anchor=W, window=label22)
+    #public key for the m/44/0/0/2 path 
+    label23 = tk.Label(root, text="Public key for m/44'/0'/0'/2: ", font=('helvetica', 10))
+    canvas1.create_window(50, 750,anchor=W, window=label23)
+    
+    label24 = tk.Label(root, text=pub3, font=('helvetica', 10, 'bold'))
+    canvas1.create_window(250, 750,width=800,anchor=W, window=label24)
     return
+
+
+# After the button click 
+def compute():
+    clean()
+    value = entry1.get()
+    
+    entropy,mnemonic,seed,bip32,xprv,xpub,prv1,pub1,prv2,pub2,prv3,pub3=main(value)
+    bitSeed= byte_to_bit(seed)
+    bitEntropy=byte_to_bit(entropy)
+    viewer(bitEntropy,mnemonic,bitSeed,bip32,xprv,xpub,prv1,pub1,prv2,pub2,prv3,pub3)
+    return
+
+#After the clean button clicked
+def clean():
+    bitEntropy=""
+    mnemonic=""
+    bitSeed=""
+    xprv=""
+    bip32=""
+    xpub=""
+    xprv=""
+    prv1=""
+    pub1=""
+    prv2=""
+    pub2=""
+    prv3=""
+    pub3=""
+    viewer(bitEntropy,mnemonic,bitSeed,bip32,xprv,xpub,prv1,pub1,prv2,pub2,prv3,pub3)
+    return
+#The generate button 
+button1 = tk.Button(text='Generate the seed', command=compute, bg='green', fg='white', font=('helvetica', 9, 'bold'))
+canvas1.create_window(400, 150, window=button1)
+
+#the clean button 
+button2 = tk.Button(text='Clean', command=clean, bg='red', fg='white', font=('helvetica', 9, 'bold'))
+canvas1.create_window(500, 150, window=button2)
+
+
+root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
 
 # main()
 
 
-root = Tk()
-frm = ttk.Frame(root, padding=20)
-frm.grid()
-ttk.Label(frm, text="Experiment").grid( row=0)
-ttk.Button(frm, text="Generate Entropy", command=main).grid(column=0, row=1)
-# ttk.Button(frm, text="Generate Mnemonic", command=generateEntropy).grid(column=0, row=2)
-# ttk.Button(frm, text="Generate seed", command=generateEntropy).grid(column=0, row=3)
-# ttk.Button(frm, text="Generate Extended Privatekey", command=generateEntropy).grid(column=0, row=4)
-# ttk.Button(frm, text="Generate Extended Public Key", command=generateEntropy).grid(column=0, row=5)
-# ttk.Button(frm, text="Generate Private Key", command=generateEntropy).grid(column=0, row=6)
-# ttk.Button(frm, text="Generate Entropy From The Mnemonic", command=generateEntropy).grid(column=0, row=7)
-# ttk.Button(frm, text="Generate Public Key", command=generateEntropy).grid(column=0, row=8)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=2)
-root.mainloop()
+# root = Tk()
+# frm = ttk.Frame(root, padding=20)
+# frm.grid()
+# ttk.Label(frm, text="Experiment").grid( row=0)
+# canvas1 = ttk.Canvas(root, width=400, height=300)
+# canvas1.pack()
+# entry1 = ttk.Entry(root)
+
+# ttk.Button(frm, text="Generate Entropy", command=main).grid(column=0, row=1)
+# ttk.Button(frm, text="Generate Mnemonic", command=print(entry1)).grid(column=0, row=2)
+
+# ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=2)
+# root.mainloop()
 
 
 
